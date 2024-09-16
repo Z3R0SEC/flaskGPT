3e38bd010c5ae8c595e6efb67f3dca58c64f410c
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2024-09-16 02:19:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from chatgpt import get_response
 
 app = Flask(__name__)
@@ -7,11 +7,22 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-# Define route for home page
 @app.route("/get", methods=["GET", "POST"])
 def gpt_response():
     userText = request.args.get('msg')
     return str(get_response(userText))
+
+@app.route('/sitemap.xml')
+def sitemap():
+    sitemap_xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>http://lwandle-ai.x10.bz/</loc>
+    <lastmod>2023-10-05</lastmod>
+    <priority>1.0</priority>
+  </url>
+</urlset>'''
+    return Response(sitemap_xml, mimetype='application/xml')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=False)
